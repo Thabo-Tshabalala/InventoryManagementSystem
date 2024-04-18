a43581d2f3165bf970d90f0cb89538bae1466752
Thabo <justicethabo21@gmail.com>
2024-04-18 15:27:49 +0200

    User domain done

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -1,4 +1,113 @@
 package za.ac.cput.domain;
 
+import java.util.Objects;
+
 public class User {
+
+    private String userID;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private boolean role;
+
+
+
+    protected User(){}
+    public User(Builder builder){
+        this.userID = builder.userID;
+        this.name = builder.name;
+        this.surname = builder.surname;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.role = builder.role;
+
+    }
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isRole() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return role == user.role && Objects.equals(userID, user.userID) && Objects.equals(name, user.name) && Objects.equals(surname, user.surname) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userID, name, surname, email, password, role);
+    }
+
+    public static class Builder{
+        private String userID;
+        private String name;
+        private String surname;
+        private String email;
+        private String password;
+        private boolean role;
+
+        public Builder setUserID(String userID) {
+            this.userID = userID;
+            return  this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return  this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setRole(boolean role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder copy(User user){
+            this.userID = user.userID;
+            this.name = user.name;
+            this.surname = user.surname;
+            this.email = user.email;
+            this.password = user.password;
+            this.role = user.role;
+            return this;
+        }
+        public User build(){return  new User(this);
+        }
+    }
 }
